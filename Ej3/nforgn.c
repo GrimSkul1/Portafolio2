d620d5172a90f2e844019fd1b00ed99ae921a011
GrimSkul1 <54964499+GrimSkul1@users.noreply.github.com>
2019-12-07 17:40:28 -0400

    .

--- Ej3/nforgn.c
+++ Ej3/nforgn.c
@@ -22,19 +22,20 @@ void sort (void *base, size_t nitems, size_t size, int (*compar)(const void *, c
 }
 
 int charcompr (const void *num1, const void *num2){
-    char comp1 = (char)num1;
-    char comp2 = (char)num2;
-    return comp2 - comp1;
-}
-
-int intcompr (const void *num1, const void *num2){
-    int comp1 =  (int) num1;
-    int comp2 =  (int) num2;
+    char comp1 = (int)num1;
+    char comp2 = (int)num2;
     return comp2 - comp1;
 }
 
 int floatcompr (const void *num1, const void *num2){
-    float comp1 = (float) num1;
-    float comp2 = (float) num2;
-    return comp2 - comp1;
+    float comp1 = (int) num1;
+    float comp2 = (int) num2;
+    return  comp2 - comp1;
 }
+
+int intcompr (const void *num1, const void *num2){
+    int comp1 =  num1;
+    int comp2 =  num2;
+    return  comp2 - comp1;
+}
+
